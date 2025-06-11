384e5a3b20bdbf7bef4c216f81dc56760a50d08c
vaishnaviselvaraj05 <srivaishnavis009@gmail.com>
2025-06-11 20:51:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Prime Number/prime-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    bool isPrime(int n) {
+        if(n <= 1) return false;
+        if(n == 2) return true;
+        if(n % 2 == 0) return false;
+        int m = sqrt(n);
+        for(int i = 3 ; i <= m ; i += 2)
+        {
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+};
