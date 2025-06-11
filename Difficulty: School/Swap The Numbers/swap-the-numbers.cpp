977e4f62c6b3478e57496d319a5f2d3634554104
vaishnaviselvaraj05 <srivaishnavis009@gmail.com>
2025-06-11 20:59:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: School/Swap The Numbers/swap-the-numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    // Write Code to Swap
+    
+    swap(a,b);
+
+    cout << a << " " << b << endl;
+    return 0;
+}
