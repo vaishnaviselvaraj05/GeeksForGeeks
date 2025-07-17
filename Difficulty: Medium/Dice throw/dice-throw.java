21c44662304fdd7ae3a61b7288b1ef44fd3d03dd
vaishnaviselvaraj05 <srivaishnavis009@gmail.com>
2025-07-17 15:33:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dice throw/dice-throw.java
+++ Difficulty: Medium/Dice throw/dice-throw.java
@@ -2,9 +2,7 @@ class Solution {
     static int noOfWays(int m, int n, int x) {
         // code here
         int[][] dp = new int[n + 1][x + 1];
-
         dp[0][0] = 1;
-
         for (int dice = 1; dice <= n; dice++) {
             for (int sum = 1; sum <= x; sum++) {
                 for (int face = 1; face <= m; face++) {
